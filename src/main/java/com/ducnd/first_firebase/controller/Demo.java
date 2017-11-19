a4b29c5dc37237e02432fb09693791353cde8b18
Dinh-Duc Nguyen <ducn.nguyen@vmodev.com>
2017-11-20 00:37:45 +0700

    no comment

--- src/main/java/com/ducnd/first_firebase/controller/Demo.java
+++ src/main/java/com/ducnd/first_firebase/controller/Demo.java
@@ -5,20 +5,26 @@ import com.google.cloud.WriteChannel;
 import com.google.cloud.storage.*;
 import com.google.firebase.cloud.StorageClient;
 import com.google.firebase.internal.FirebaseAppStore;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.DefaultResourceLoader;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.nio.ByteBuffer;
 
 @RestController
 public class Demo {
 
-    @GetMapping("/")
-    public String getAppName() {
-        Bucket bucket = StorageClient.getInstance().bucket();
+    private Storage storage;
+
+    @Autowired
+    public Demo() {
         try {
+            Bucket bucket = StorageClient.getInstance().bucket();
             InputStream inputStream = new FileInputStream(new File("image.png"));
             BlobInfo blobInfo =
                     BlobInfo.newBuilder(bucket.getName(), "image.png")
@@ -28,13 +34,30 @@ public class Demo {
             File file = new DefaultResourceLoader().getResource("firstproject1994-firebase-adminsdk-qn2kc-66400afe59.json").getFile();
             FileInputStream serviceAccount =
                     new FileInputStream(file);
-            Storage storage = StorageOptions.newBuilder()
+            storage = StorageOptions.newBuilder()
                     .setProjectId("firstproject1994")
                     .setCredentials(ServiceAccountCredentials.fromStream(serviceAccount))
                     .build()
                     .getService();
+        } catch (IOException e) {
+            storage = null;
+        }
+
+    }
+
+    @PostMapping(value = "/postImage")
+    public String getAppName(
+            @RequestParam(value = "image") MultipartFile image
+    ) {
+        try {
+            BlobInfo blobInfo =
+                    BlobInfo.newBuilder(StorageClient.getInstance().bucket(),
+                            RandomStringUtils.random(30, "1234567890lkasjfhlkasfjalkjdfh34568798alskdjfh796ftasdoa8soifuglajksdbfafaoisdulfkh"))
+                            .setContentType(image.getContentType())
+                            .build();
             WriteChannel writeChannel = storage.writer(blobInfo);
             byte[] b = new byte[1024];
+            InputStream inputStream = image.getInputStream();
             int le = inputStream.read(b);
             while (le >= 0) {
                 writeChannel.write(ByteBuffer.wrap(b, 0, le));
@@ -42,19 +65,25 @@ public class Demo {
             }
             inputStream.close();
             writeChannel.close();
-            writeChannel.close();
 
-            File file1 = new File("hihi.png");
-            FileOutputStream stream = new FileOutputStream(file1);
-            stream.write(
-                    StorageClient.getInstance().bucket().get("image.png").getContent()
-            );
-            stream.close();
-            System.out.println(
-                    "entry: " + StorageClient.getInstance().bucket().listAcls().get(0).getEntity().toString());
+//            File file1 = new File("hihi.png");
+//            FileOutputStream stream = new FileOutputStream(file1);
+//            stream.write(
+//                    StorageClient.getInstance().bucket().get("image.png").getContent()
+//            );
+//            stream.close();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
         return "First Project Firebase";
+    }
+
+    @GetMapping(value = "/getImage",  produces = MediaType.IMAGE_JPEG_VALUE)
+    public @ResponseBody
+    byte[] getImage(
+            @RequestParam(value = "image") String image
+    ) {
+        return StorageClient.getInstance().bucket().get(image).getContent();
     }
 }
